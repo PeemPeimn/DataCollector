5d576c0ef6aa0cfb51dd2fe7d10a99170632d7fa
peem <intamoon.peem@outlook.com>
2024-06-20 03:14:53 +0700

    feat: add commit message log

--- .github/workflows/check_commit_message.py
+++ .github/workflows/check_commit_message.py
@@ -2,8 +2,11 @@ import re
 import sys
 
 regex = "^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\([\w\-\.]+\))?(!)?: ([\w ])+([\s\S]*)"
-txt = sys.argv[1]
-match = re.search(regex, txt)
+
+commit_message = sys.argv[1]
+print(f'Commit message: "{commit_message}"')
+
+match = re.search(regex, commit_message)
 
 if match is None:
   print("The commit message does not follow the Conventional Commits specification.")
