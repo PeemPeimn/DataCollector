fe027458dee2981e39bd8b1c8fade67e818c8865
peem <intamoon.peem@outlook.com>
2024-06-30 01:11:12 +0700

    fix(database): add a created_at field to both reddit tables

--- Database/SqlScripts/1-reddit.sql
+++ Database/SqlScripts/1-reddit.sql
@@ -1,9 +1,13 @@
-CREATE TABLE
-  reddit_posts (id INTEGER PRIMARY KEY, post jsonb NOT NULL);
+CREATE TABLE reddit_posts(
+  id integer PRIMARY KEY,
+  created_at timestamp with time zone NOT NULL,
+  post jsonb NOT NULL
+);
 
-CREATE TABLE
-  reddit_data (
-    id SERIAL PRIMARY KEY,
-    post_id INTEGER REFERENCES reddit_posts (id),
-    messages jsonb NOT NULL
-  );
+CREATE TABLE reddit_data(
+  id serial PRIMARY KEY,
+  post_id integer REFERENCES reddit_posts(id),
+  created_at timestamp with time zone NOT NULL,
+  messages jsonb NOT NULL
+);
+
