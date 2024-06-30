a9a5087cc25ef58695151dd2690e02246cad5970
peem <intamoon.peem@outlook.com>
2024-06-30 20:56:57 +0700

    fix(database): change post_id type and add constraint ON DELETE CASCADE on the child table

--- Database/SqlScripts/1-reddit.sql
+++ Database/SqlScripts/1-reddit.sql
@@ -1,12 +1,12 @@
 CREATE TABLE reddit_posts(
-  id integer PRIMARY KEY,
+  id text PRIMARY KEY,
   created_at timestamp with time zone NOT NULL,
   post jsonb NOT NULL
 );
 
 CREATE TABLE reddit_data(
   id serial PRIMARY KEY,
-  post_id integer REFERENCES reddit_posts(id),
+  post_id text REFERENCES reddit_posts(id) ON DELETE CASCADE,
   created_at timestamp with time zone NOT NULL,
   messages jsonb NOT NULL
 );
